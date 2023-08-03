b0b71fb37679fe69591c57ec2a6ca17ce7a93bc0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-03 23:11:01 +0600

    Question 4 solved

---
+++ Assignment 1/Question_4.cpp
@@ -0,0 +1,39 @@
+// (a)
+
+// int count = 0;
+// for (int i = n; i > 0; i /= 2) 
+// {
+//     for (int j = 0; j < n; j+=5) 
+//    {
+//         count += 1;
+//     }
+// }
+
+// (b)
+
+// for(int i =1; i*i<n; i++)
+// {
+//      cout << “hello”;
+// }
+
+// (c)
+
+// for(int i =1; i<n; i=i*2)
+// {
+//    for(int j=1; j*j<n; j+=2)
+//   {
+//       cout << “hello”;
+//    }
+// }
+
+// (d)
+// int m = 1;
+// for(int i=0; m<=n; i++)
+// {
+//     m+=i;
+// }
+
+// (a) O(n * log n)
+// (b) O(sqrt(n))
+// (c) O(sqrt(n) * log n)
+// (d) O(sqrt(n))
