c13435287d3daba6dfdf87b94bbad1c6f2b40852
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-07 21:20:09 +0600

    adjacency with weigheted

---
+++ Module 6/Adjacency_mat_Weighted.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3+5;
+int adjmat[N][N];
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+    {
+        int u,v,w;
+        cin>>u>>v>>w;
+        adjmat[u][v]=w; // directed
+        adjmat[v][u]=w; // undirected
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<adjmat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
