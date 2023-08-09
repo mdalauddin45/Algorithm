02b681e3b61865ddae15954f3477b4ef55b18769
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-10 01:26:59 +0600

    problem 3 solved

---
+++ Module 6.5/Problem_3.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+5;
+vector<int>adj[N];
+int adjmat[N][N];
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j:adj[i])
+        {
+            adjmat[i][j]=1;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<adjmat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
