059fde2a59a307521057231f179916d3ef4c6289
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-11 22:20:06 +0600

    Know Your Level solved

--- Assignment 2/Know_Your_Level.cpp
+++ Assignment 2/Know_Your_Level.cpp
@@ -1,47 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e3+5;
+const int N = 1e5 + 5;
 vector<int> adj[N];
 bool visited[N];
-int level[N];
-void dfs(int u)
-{
-    visited[u]=true;
-    for(int v:adj[u])
-    {
-        if(!visited[v])
-        {
-            level[v]=level[u]+1;
-            dfs(v);
+void bfs(int s,int level) {
+    queue<pair<int,int>>q;
+    vector<int>shops;
+    q.push({s, 0});
+    visited[s] = true;
+    while (!q.empty()) {
+        int first = q.front().first;
+        int second = q.front().second;
+        q.pop();
+        if (second == level) shops.push_back(first);
+        for (int v : adj[first]) {
+            if (visited[v]) continue;
+            visited[v] = true;
+            int newLevel = second + 1;
+            q.push({v,newLevel}); 
         }
     }
+    if (!shops.empty()) {
+        sort(shops.begin(), shops.end());
+        for (int i=0;i<shops.size();i++) cout<<shops[i]<<" ";
+    } else cout<<-1;
 }
-int main()
-{
-    int n,m;
-    cin>>n>>m;
-    for(int i=1;i<=m;i++)
-    {
-        int u,v;
-        cin>>u>>v;
-        adj[u].push_back(v);
-    }
-    dfs(0);
+
+int main() {
+   int n,m;
+   cin>>n>>m;
+   for(int i=0;i<m;i++) {
+       int u,v;
+       cin>>u>>v;
+       adj[u].push_back(v);
+       adj[v].push_back(u);
+   }
     int l;
     cin>>l;
-    if(level[l]==-1)
-    {
-        cout<<"-1";
-        return 0;
-    }else{
-        
-    for(int i=0;i<n;i++)
-    {
-        if(level[i]==l)
-        {
-            cout<<i<<" ";
-        }
-    }
-    }
+    bfs(0,l);
     return 0;
 }
