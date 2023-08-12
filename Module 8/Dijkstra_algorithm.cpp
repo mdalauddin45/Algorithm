ea97fcb3f0fd7ed1cbca5f689f6496a795cdf1f2
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-12 21:59:39 +0600

    Dijkstra implimentation

---
+++ Module 8/Dijkstra_algorithm.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+5;
+const int INF=1e9+10;
+
+typedef pair<int,int> pii;
+vector<pii> adj[N];
+vector<int> dist(N,INF);
+vector<bool> visited(N);
+void dijkstra(int s)
+{
+    priority_queue<pii,vector<pii>,greater<pii>>pq;
+    dist[s]=0;
+    pq.push({dist[s],s});
+    while(!pq.empty())
+    {
+        int u=pq.top().second;
+        pq.pop();
+        
+        visited[u]=true;
+        for(pii vpair:adj[u])
+        {
+            int v = vpair.first;
+            int w = vpair.second;
+            if(visited[v]) continue;
+            if(dist[v]>dist[u]+w)
+            {
+                dist[v]=dist[u]+w;
+                pq.push({dist[v],v});
+            }
+        }
+    }
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++)
+    {
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w}); // for undirected graph
+    }
+    dijkstra(1);
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"Distance of "<<i<<": "<<dist[i]<<endl;
+    }
+    return 0;
+}
