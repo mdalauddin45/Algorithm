41242e52625c4f52fbbd990fd3cf52a9727b6c4b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-12 21:08:14 +0600

    shortest path

---
+++ Module 8/Shortes_bfs.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3+5;
+vector<int> adj[N];
+bool visited[N];
+int level[N];
+void bfs(int s)
+{
+    queue<int>q;
+    q.push(s);
+    visited[s]=true;
+    level[s]=0;
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();
+        for(int v:adj[u])
+        {
+            if(visited[v]) continue;
+            visited[v]=true;
+            level[v]=level[u]+1;
+            q.push(v);
+        }
+    }
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // for undirected graph
+    }
+    int s,d;
+    cin>>s>>d;
+    bfs(s);
+    cout<<level[d]<<endl;
+
+    return 0;
+}
