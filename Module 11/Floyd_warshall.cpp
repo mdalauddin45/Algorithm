8e931328897d7665e398fb3c56eeb2dcbdeb14a3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-16 12:40:32 +0600

    not solved

---
+++ Module 11/Floyd_warshall.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+int main()
+{
+    cin>>n>>m;
+    while (m--)
+    {
+        int u,v,w;
+        cin>>u>>v>>w;
+        d[u][v]=w;
+        // d[v][u]=w;
+    }
+    
+    return 0;
+}
+
+
