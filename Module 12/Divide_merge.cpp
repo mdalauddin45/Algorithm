766472a6365db5f775080290454b8b7610e04b88
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-21 03:18:08 +0600

    divide and merrge

---
+++ Module 12/Divide_merge.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int a[],int l,int m,int r)
+{
+     int leftSize = m-l+1;
+    int rightSize = r-m;
+    int L[leftSize],R[rightSize];
+    int k=0;
+    for(int i=l;i<=m;i++)
+    {
+        L[k]=a[i];
+        k++;
+    }
+    k=0;
+    for(int i=m+1;i<=r;i++)
+    {
+        R[k]=a[i];
+        k++;
+    }
+    int i=0,j=0;
+    int cur=l;
+    while(i<leftSize && j<rightSize)
+    {
+        if(L[i] <= R[j])
+        {
+            a[cur]=L[i];
+            i++;
+        }
+        else 
+        {
+            a[cur]=R[j];
+            j++;
+        }
+        cur++;
+    }
+
+    while(i<leftSize)
+    {
+        a[cur]=L[i];
+        i++;
+        cur++;
+    }
+
+    while(j<rightSize)
+    {
+        a[cur]=R[j];
+        j++;
+        cur++;
+    }
+}
+void divide(int a[],int l,int r)
+{
+    if(l==r) return;
+    // for(int i=l;i<=r;i++) cout<<a[i]<<" ";
+    // cout<<endl;
+    if(l<r)
+    {
+        int mid = (l+r)/2;
+        divide(a,l,mid);
+        divide(a,mid+1,r);
+        merge(a,l,mid,r);
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    divide(a,0,n-1);
+    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+    return 0;
+}
