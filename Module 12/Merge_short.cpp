fd7215ccaa9363abf5e75facd72a03b69acbaa3c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-21 02:56:46 +0600

    merge sort

--- Module 12/Merge_short.cpp
+++ Module 12/Merge_short.cpp
@@ -1,44 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
-void merge(int arr[],int l,int mid,int r)
+void merge(int a[],int l,int m,int r)
 {
-    int ls=mid-l+1;
-    int rs=r-mid;
-    int L[ls];
-    int R[rs];
+    int leftSize = m-l+1;
+    int rightSize = r-m;
+    int L[leftSize],R[rightSize];
     int k=0;
-    for(int i=0;i<ls;i++){
-        L[k]=arr[i];
+    for(int i=l;i<=m;i++)
+    {
+        L[k]=a[i];
         k++;
     }
     k=0;
-    for(int i=0;i<rs;i++){
-        R[k]=arr[mid+1+i];
+    for(int i=m+1;i<=r;i++)
+    {
+        R[k]=a[i];
         k++;
     }
     int i=0,j=0;
-    int curr=l;
-   while (i<rs && j<ls)
-   {
-    if(L[i]<R[j])
+    int cur=l;
+    while(i<leftSize && j<rightSize)
     {
-        arr[curr]=L[i];
-        i++;
-    }else{
-        arr[curr]=R[j];
-        j++;
+        if(L[i] <= R[j])
+        {
+            a[cur]=L[i];
+            i++;
+        }
+        else 
+        {
+            a[cur]=R[j];
+            j++;
+        }
+        cur++;
     }
-    curr++;
-   }
-   
+
+    while(i<leftSize)
+    {
+        a[cur]=L[i];
+        i++;
+        cur++;
+    }
+
+    while(j<rightSize)
+    {
+        a[cur]=R[j];
+        j++;
+        cur++;
+    }
 }
 int main()
 {
     int n;
     cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
-    merge(arr,0,3,n-1);
-    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    merge(a,0,3,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
