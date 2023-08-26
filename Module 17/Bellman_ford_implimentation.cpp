f32656c9de4860a3cf07328e10bb98430ca2371f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-26 22:20:44 +0600

    bellman ford done

--- Module 17/Bellman_ford_implimentation.cpp
+++ Module 17/Bellman_ford_implimentation.cpp
@@ -1,7 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Edge{
+    public:
+        int u;
+        int v;
+        int w;
+    Edge(int u,int v,int w)
+    {
+        this->u=u;
+        this->v=v;
+        this->w=w;
+    }
+};
 int main()
 {
-    
+    int n,m;
+    cin>>n>>m;
+    vector<Edge>v;
+    while (m--)
+    {
+      int a,b,w;
+      cin>>a>>b>>w;
+      Edge ed(a,b,w);
+      v.push_back(ed);
+    //   cout<<ed.u<<" "<<ed.v<<" "<<ed.w<<endl;
+    }
+    int dis[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        dis[i]=INT_MAX;
+    }
+    dis[1]=0;
+    for(int i=1;i<=n-1;i++)
+    {
+        for(int j=0;j<v.size();j++)
+        {
+            Edge ed=v[j];
+            int a=ed.u;
+            int b=ed.v;
+            int w=ed.w;
+            if(dis[a]+w<dis[b])
+            {
+                dis[b]=dis[a]+w;
+            }
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"Node "<<i<<" "<<dis[i]<<endl;
+    }
+   
     return 0;
 }
