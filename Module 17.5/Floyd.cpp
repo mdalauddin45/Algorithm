83e4455c66b95f0a864f35a96173d35b80e9b729
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-27 22:42:18 +0600

    Floyd Solved

--- Module 17.5/Floyd.cpp
+++ Module 17.5/Floyd.cpp
@@ -1,7 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    
+const int INF = 1e9;
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> dist(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+            if (dist[i][j] == -1) {
+                dist[i][j] = INF;
+            }
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    int mxdist = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] != INF && dist[i][j] > mxdist) {
+                mxdist = dist[i][j];
+            }
+        }
+    }
+    cout << mxdist << endl;
+
     return 0;
 }
