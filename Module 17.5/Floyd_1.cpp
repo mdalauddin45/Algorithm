b0588ff1b14fe3c3f08a918fbc33b6d53983b0c5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-08-27 22:32:59 +0600

    Floyd_1 solved

---
+++ Module 17.5/Floyd_1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> dist(n + 1, vector<int>(n + 1));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> dist[i][j];
+        }
+    }
+
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
