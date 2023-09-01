b7732b653faea14b68aa848e2afb930a857667c0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-02 00:09:50 +0600

    can go again solved

--- Assignment 4/Can_Go_Again.cpp
+++ Assignment 4/Can_Go_Again.cpp
@@ -1,128 +1,71 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define pi pair<int, int>
 using namespace std;
-int parent[1000];
-int parentLevel[1000];
-class Edge{
-    public:
-        int a,b,w;
-        Edge(int a,int b,int w)
-        {
-            this->a=a;
-            this->b=b;
-            this->w=w;
-        }
+const long long int INF = 1e18;
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
 };
-bool cmp(Edge a,Edge b)
-{
-    return a.w<b.w;
-}
-void dsu_set(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        parent[i] = -1;
-        parentLevel[i] = 0;
-    }
-}
-int dsu_find(int node)
-{
-    while (parent[node] != -1)
-    {
-        node = parent[node];
-    }
-    return node;
-}
-void dsu_union(int a, int b)
-{
-    int leaderA = dsu_find(a);
-    int leaderB = dsu_find(b);
-    if (leaderA != leaderB)
-    {
-        if (parentLevel[leaderA] > parentLevel[leaderB])
-        {
-            parent[leaderB] = leaderA;
-        }
-        else if (parentLevel[leaderB] > parentLevel[leaderA])
-        {
-            parent[leaderA] = leaderB;
-        }
-        else
-        {
-            parent[leaderB] = leaderA;
-            parentLevel[leaderA]++;
-        }
-    }
-}
+
 int main()
 {
     int n,m;
     cin>>n>>m;
     vector<Edge>v;
-    vector<Edge>ans;
-    dsu_set(n);
     while (m--)
     {
-        int a,b,w;
-        cin>>a>>b>>w;
-        v.push_back(Edge(a,b,w));
+        int a,b;
+        long long int w;
+        cin >>a>>b>>w;
+        Edge ed(a,b,w);
+        v.push_back(ed);
     }
-    for(Edge val:v)
-    {
-        // cout<<val.a<<" "<<val.b<<" "<<val.w<<endl;
-        int a=val.a;
-        int b=val.b;
-        int w=val.w;
-        int leaderA=dsu_find(a);
-        int leaderB=dsu_find(b);
-        if(leaderA==leaderB) continue;
-        ans.push_back(val);
-        dsu_union(a,b);
-    }
-
-    int s;
+    long long int s;
     cin>>s;
+    long long int dist[n + 1];
+    for (int i=1;i<=n;i++) dist[i] = INF;
+    dist[s] = 0;
+    for (int i=1;i<n;i++)
+    {
+        for (int j=0;j<v.size();j++)
+        {
+            Edge ed = v[j];
+            int a = ed.a;
+            int b = ed.b;
+            long long int w = ed.w;
+            if (dist[ed.a] !=INF && dist[ed.a] + ed.w<dist[ed.b]) dist[ed.b] = dist[ed.a] + ed.w;
+        }
+    }
+    bool flag = false;
+    for (int j=0;j<v.size();j++)
+    {
+        Edge ed = v[j];
+        int a = ed.a;
+        int b = ed.b;
+        long long int w = ed.w;
+        if (dist[a] != INF && dist[a] + w < dist[b]) flag = true;
+    }
 
     int q;
-    cin>>q;
-
-    for (int i =0;i<q;i++) {
+    cin>> q;
+    while (q--)
+    {
         int d;
         cin>>d;
-
-        vector<int> dist(n + 1, INT_MAX);
-        dist[s] = 0;
-
-        for (int j = 0; j < n - 1; j++) {
-            for (Edge edge : v) {
-                int u = edge.a;
-                int v = edge.b;
-                int w = edge.w;
-                if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
-                }
-            }
-        }
-
-        bool negativeCycle = false;
-        for (Edge edge : v) {
-            int u = edge.a;
-            int v = edge.b;
-            int w = edge.w;
-            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
-                negativeCycle = true;
-                break;
-            }
-        }
-
-        if (negativeCycle) {
-            cout << "Negative Cycle Detected" << endl;
+        if (flag)
+        {
+            cout<<"Negative Cycle Detected "<<endl;
             break;
-        } else if (dist[d] == INT_MAX) {
-            cout << "Not Possible" << endl;
-        } else {
-            cout << dist[d] << endl;
         }
+        else if (dist[d] == INF) cout<<"Not Possible "<<endl;
+        else cout<<dist[d]<<endl;
     }
-
     return 0;
 }
