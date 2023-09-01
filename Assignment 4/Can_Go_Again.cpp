9b00a1b653c8b888f3e4ec04fa3ae99aab710df5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-01 16:03:13 +0600

    p4 case error

--- Assignment 4/Can_Go_Again.cpp
+++ Assignment 4/Can_Go_Again.cpp
@@ -1,58 +1,128 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e3+5;
-const int INF=1e9+10;
-typedef pair<int,int> pii;
-vector<vector<pii>>adj;
-vector<int> dist;
-vector<bool> visited;
-void dijkstra(int s)
-{
-    priority_queue<pii,vector<pii>,greater<pii>>pq;
-    dist[s]=0;
-    pq.push({dist[s],s});
-    while(!pq.empty())
-    {
-        int x=pq.top().second;
-        int y=pq.top().first;
-        pq.pop();
-        if(visited[x]) continue;
-        visited[x]=true;
-        for(auto u:adj[x])
+int parent[1000];
+int parentLevel[1000];
+class Edge{
+    public:
+        int a,b,w;
+        Edge(int a,int b,int w)
         {
-            int v=u.first;
-            int w=u.second;
-            if(dist[v]>dist[x]+w)
-            {
-                dist[v]=dist[x]+w;
-                pq.push({dist[v],v});
-            }
+            this->a=a;
+            this->b=b;
+            this->w=w;
         }
-
+};
+bool cmp(Edge a,Edge b)
+{
+    return a.w<b.w;
+}
+void dsu_set(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        parentLevel[i] = 0;
+    }
+}
+int dsu_find(int node)
+{
+    while (parent[node] != -1)
+    {
+        node = parent[node];
+    }
+    return node;
+}
+void dsu_union(int a, int b)
+{
+    int leaderA = dsu_find(a);
+    int leaderB = dsu_find(b);
+    if (leaderA != leaderB)
+    {
+        if (parentLevel[leaderA] > parentLevel[leaderB])
+        {
+            parent[leaderB] = leaderA;
+        }
+        else if (parentLevel[leaderB] > parentLevel[leaderA])
+        {
+            parent[leaderA] = leaderB;
+        }
+        else
+        {
+            parent[leaderB] = leaderA;
+            parentLevel[leaderA]++;
+        }
     }
 }
 int main()
 {
     int n,m;
     cin>>n>>m;
-    for(int i=0;i<m;i++)
+    vector<Edge>v;
+    vector<Edge>ans;
+    dsu_set(n);
+    while (m--)
     {
-        int u,v,w;
-        cin>>u>>v>>w;
-        adj[u].push_back({v,w});
+        int a,b,w;
+        cin>>a>>b>>w;
+        v.push_back(Edge(a,b,w));
     }
-    int s,t;
-    cin>>s>>t;
-    dijkstra(s);
-    while (t--)
+    for(Edge val:v)
     {
+        // cout<<val.a<<" "<<val.b<<" "<<val.w<<endl;
+        int a=val.a;
+        int b=val.b;
+        int w=val.w;
+        int leaderA=dsu_find(a);
+        int leaderB=dsu_find(b);
+        if(leaderA==leaderB) continue;
+        ans.push_back(val);
+        dsu_union(a,b);
+    }
+
+    int s;
+    cin>>s;
+
+    int q;
+    cin>>q;
+
+    for (int i =0;i<q;i++) {
         int d;
         cin>>d;
-        if( dist[d]==INF)
-            cout<<"Not Possible"<<endl;
-        else
-            cout<<dist[d]<<endl;
+
+        vector<int> dist(n + 1, INT_MAX);
+        dist[s] = 0;
+
+        for (int j = 0; j < n - 1; j++) {
+            for (Edge edge : v) {
+                int u = edge.a;
+                int v = edge.b;
+                int w = edge.w;
+                if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+
+        bool negativeCycle = false;
+        for (Edge edge : v) {
+            int u = edge.a;
+            int v = edge.b;
+            int w = edge.w;
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                negativeCycle = true;
+                break;
+            }
+        }
+
+        if (negativeCycle) {
+            cout << "Negative Cycle Detected" << endl;
+            break;
+        } else if (dist[d] == INT_MAX) {
+            cout << "Not Possible" << endl;
+        } else {
+            cout << dist[d] << endl;
+        }
     }
-    
+
     return 0;
 }
--- Assignment 4/p2.cpp
+++ Assignment 4/p2.cpp
@@ -1,57 +1,53 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <limits>
+
 using namespace std;
 
-const int INF = 1e9;
+const int INF = numeric_limits<int>::max();
 
 int main() {
-    int n, e;
-    cin >> n >> e;
+    int N, E;
+    cin >> N >> E;
 
-    vector<vector<pair<int, int>>> graph(n + 1);
+    // Initialize the distance matrix with INF
+    vector<vector<int>> distance(N + 1, vector<int>(N + 1, INF));
 
-    for (int i = 0; i < e; i++) {
-        int a, b, w;
-        cin >> a >> b >> w;
-        graph[a].emplace_back(b, w);
+    // Initialize the diagonal elements with 0
+    for (int i = 1; i <= N; ++i) {
+        distance[i][i] = 0;
     }
 
-    int q;
-    cin >> q;
+    // Read edge weights and populate the distance matrix
+    for (int i = 0; i < E; ++i) {
+        int A, B, W;
+        cin >> A >> B >> W;
+        distance[A][B] = W;
+    }
 
-    while (q--) {
-        int x, y;
-        cin >> x >> y;
-
-        vector<int> distance(n + 1, INF);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        distance[x] = 0;
-        pq.emplace(0, x);
-
-        while (!pq.empty()) {
-            int dist = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            if (dist > distance[node]) {
-                continue;
-            }
-
-            for (auto neighbor : graph[node]) {
-                int next_node = neighbor.first;
-                int edge_weight = neighbor.second;
-
-                if (distance[node] + edge_weight < distance[next_node]) {
-                    distance[next_node] = distance[node] + edge_weight;
-                    pq.emplace(distance[next_node], next_node);
+    // Perform the Floyd-Warshall algorithm
+    for (int k = 1; k <= N; ++k) {
+        for (int i = 1; i <= N; ++i) {
+            for (int j = 1; j <= N; ++j) {
+                if (distance[i][k] != INF && distance[k][j] != INF) {
+                    distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
                 }
             }
         }
+    }
 
-        if (distance[y] == INF) {
+    int Q;
+    cin >> Q;
+
+    // Answer the queries
+    for (int q = 0; q < Q; ++q) {
+        int X, Y;
+        cin >> X >> Y;
+        
+        if (distance[X][Y] == INF) {
             cout << -1 << endl;
         } else {
-            cout << distance[y] << endl;
+            cout << distance[X][Y] << endl;
         }
     }
 
