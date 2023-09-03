a907bd25dfc34bd30d8311056c82afe26dbf2762
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-03 16:10:22 +0600

    solved A frog

--- Module 21/A_Frog_1.cpp
+++ Module 21/A_Frog_1.cpp
@@ -1,27 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e5+5;
-int dp[N];
-int solve(int arr[],int n)
-{
-    if(dp[n]!=-1) return dp[n];
-    if(n==1) return 0;
-    else if(n==2) return dp[n]= abs(arr[2]-arr[1]);
-    else{
-        int choice1=solve(arr,n-1)+abs(arr[n]-arr[n-1]);
-        int choice2=solve(arr,n-2)+abs(arr[n]-arr[n-2]);
-        return dp[n]=min(choice1,choice2);
-    }
-}
 int main()
 {
     int n;
     cin>>n;
-    int arr[n+1];
-    for(int i=1;i<=n;i++) {
-        dp[i]=-1;
-        cin>>arr[i];
-        }
-    cout<<solve(arr,n)<<endl;   
+    int cost[n+1],dp[n+1];
+    for(int i=1;i<=n;i++) cin>>cost[i];
+    dp[1]=0;
+    dp[2]=abs(cost[2]-cost[1]);
+    for(int i=3;i<=n;i++)
+    {
+        int choice1=dp[i-1]+abs(cost[i]-cost[i-1]);
+        int choice2=dp[i-2]+abs(cost[i]-cost[i-2]);
+        dp[i]=min(choice1,choice2);
+    }
+    cout<<dp[n]<<endl;
     return 0;
 }
