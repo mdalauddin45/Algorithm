d86e25bf422b9d4883ef8e2f351511d3db8d16a3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-08 00:18:16 +0600

    not solved

---
+++ Assignment 5/Exam_Marks.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        int arr[n];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        bool dp[n+1][m+1];
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        if(dp[n][m]) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
