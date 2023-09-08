3539f7ab771716332b9c7490fea9752ddcc9bf9b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-08 09:03:45 +0600

    chocolates solved

--- Assignment 5/Chocolates.cpp
+++ Assignment 5/Chocolates.cpp
@@ -6,34 +6,38 @@ int main()
     cin>>t;
     while (t--)
     {
-        int n;
+        int n,s=0;
         cin>>n;
-        int arr[n],s=0;
-        for(int i=0;i<n;i++)
-        {
+        int arr[n];
+        for(int i=0;i<n;i++){
             cin>>arr[i];
             s=s+arr[i];
         }
         if(s%2==0)
         {
             int mid=s/2;
-        vector<vector<bool>> dp(n + 1, vector<bool>(mid + 1, false));
-            for(int i=1;i<=n;i++) dp[i][0] = true;
-
+            bool dp[n+1][mid+1];
+            for(int i=0;i<=n;i++){
+                for(int j=0;j<=mid;j++){
+                    dp[i][j]=false;
+                }
+            }
+            for(int i=0;i<=n;i++) dp[i][0]=true;
             for(int i=1;i<=n;i++){
                 for(int j=0;j<=mid;j++){
-                    if(arr[i-1]<=j) {
-                        dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                    if(arr[i-1]<=j){
+                        dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
                     }else{
-                        dp[i][j] = dp[i-1][j];
+                        dp[i][j]=dp[i-1][j];
                     }
                 }
             }
-            if(dp[n][s/2]) cout<<"YES"<<endl;
+            if(dp[n][mid]) cout<<"YES"<<endl;
             else cout<<"NO"<<endl;
         }else{
             cout<<"NO"<<endl;
         }
+
     }
     
     return 0;
