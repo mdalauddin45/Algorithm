61de1d1a70689e42c4ca6764802a32062ebeb480
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-08 08:33:29 +0600

    Adventure solved

--- Assignment 5/Adventure.cpp
+++ Assignment 5/Adventure.cpp
@@ -8,15 +8,19 @@ int main()
     {
         int n,m;
         cin>>n>>m;
-        vector<int>w(n);
-        vector<int>v(n);
+        int w[n], v[n];
         for(int i=0;i<n;i++) cin>>w[i];
         for(int i=0;i<n;i++) cin>>v[i];
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for(int i=1;i<=n;i++){
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
+                dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                 if(w[i-1]<=j){
-                     dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + v[i - 1]);
+                dp[i][j] = max(v[i - 1] + dp[i - 1][j - w[i - 1]], dp[i - 1][j]);
                 }else{
                     dp[i][j] = dp[i-1][j];
                 }
