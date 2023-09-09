206efd62ca79d363cd2cc1c5c3c5c7875682e562
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-09 23:14:58 +0600

    top down solved

--- Module 25/unbounded_knapsack.cpp
+++ Module 25/unbounded_knapsack.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int dp[1005][1005];
 int unbounded_knapsack(int n,int s,int val[],int w[])
 {
     if(n==0 || s==0) return 0;
+    if(dp[n][s]!=-1) return dp[n][s];
     if(w[n-1]<=s){
     int op1=val[n-1]+unbounded_knapsack(n,s-w[n-1],val,w);
     int op2=unbounded_knapsack(n-1,s,val,w);
-    return max(op1,op2);
+    return dp[n][s] = max(op1,op2);
     }else{
-    return unbounded_knapsack(n-1,s,val,w);
+    return dp[n][s] = unbounded_knapsack(n-1,s,val,w);
     }
 }
 int main()
@@ -16,6 +18,13 @@ int main()
     int n,s;
     cin>>n>>s;
     int val[n],w[n];
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=s;j++)
+        {
+            dp[i][j]=-1;
+        }
+    }
     for(int i=0;i<n;i++) cin>>val[i];
     for(int i=0;i<n;i++) cin>>w[i];
     cout<<unbounded_knapsack(n,s,val,w);
