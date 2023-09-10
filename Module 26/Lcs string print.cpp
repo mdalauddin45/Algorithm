1a8a48439c8bb51ff0076c9bc898a8f5f7f482d5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-11 00:33:04 +0600

    lcs string print

---
+++ Module 26/Lcs string print.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    int n=a.size();
+    int m=b.size();
+    int dp[n+1][m+1];
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            dp[i][j]=-1;
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++)
+        {
+            if(a[n-1]!=b[m-1]){
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }else{
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+        }
+    }
+    int i = n, j = m;
+    string lcs;
+
+    while (i > 0 && j > 0) {
+        if (a[i - 1] == b[j - 1]) {
+            lcs = a[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    cout <<lcs << endl;
+
+    return 0;
+}
