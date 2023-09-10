9ae0e7c0955c77cd0766c8951456eee3e72560f5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-10 10:25:01 +0600

    count of coin change

--- Module 25/count_of_coin_change.cpp
+++ Module 25/count_of_coin_change.cpp
@@ -8,13 +8,13 @@ int main()
     for(int i=0;i<n;i++) cin>>w[i];
     int ss;
     cin>>ss;
-    bool dp[n+1][ss+1];
+    int dp[n+1][ss+1];
     dp[0][0]=1;
-    for(int i=1;i<=ss;i++)dp[0][i]=1;
+    for(int i=1;i<=ss;i++)dp[0][i]=0;
     for(int i=1;i<=n;i++){
         for(int j=0;j<=ss;j++){
             if(w[i-1]<=j){
-                dp[i][j]=dp[i-1][j-w[i-1]] +dp[i-1][j];
+                dp[i][j]=dp[i][j-w[i-1]] + dp[i-1][j];
             }else{
                 dp[i][j]=dp[i-1][j];
             }
