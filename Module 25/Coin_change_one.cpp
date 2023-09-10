839462941e59b48cf38f06825952f6d0cccee4bf
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-10 10:17:37 +0600

    coin change one solved

---
+++ Module 25/Coin_change_one.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int w[n];
+    for(int i=0;i<n;i++) cin>>w[i];
+    int ss;
+    cin>>ss;
+    bool dp[n+1][ss+1];
+    dp[0][0]=true;
+    for(int i=1;i<=ss;i++)dp[0][i]=false;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=ss;j++){
+            if(w[i-1]<=j){
+                dp[i][j]=dp[i-1][j-w[i-1]] ||dp[i-1][j];
+            }else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    if(dp[n][ss]) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
---
+++ Module 25/count_of_coin_change.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int w[n];
+    for(int i=0;i<n;i++) cin>>w[i];
+    int ss;
+    cin>>ss;
+    bool dp[n+1][ss+1];
+    dp[0][0]=1;
+    for(int i=1;i<=ss;i++)dp[0][i]=1;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=ss;j++){
+            if(w[i-1]<=j){
+                dp[i][j]=dp[i-1][j-w[i-1]] +dp[i-1][j];
+            }else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    cout<<dp[n][ss]<<endl;
+    return 0;
+}
