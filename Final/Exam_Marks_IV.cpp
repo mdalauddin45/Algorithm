f8faffbb17764816e8c583d60d4a9df7d5d0a4d9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-15 11:44:25 +0600

    Exam Marks IV solved

--- Final/Exam_Marks_IV.cpp
+++ Final/Exam_Marks_IV.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int MOD = 1e9+7;
 int main()
 {
     int t;
@@ -9,26 +10,27 @@ int main()
         int n,m;
         cin>>n>>m;
         int w[n];
+        int s=1000;
         for(int i=0;i<n;i++) cin>>w[i];
-        int dp[n+1][m+1];
+        int dp[n+1][s+1];
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j]=INT_MIN;
+            for(int j=0;j<=s;j++){
+                dp[i][j]=0;
             }
         }
-        for(int i=0;i<=n;i++) dp[i][0]=0;
+        dp[0][0]=1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+            for(int j=0;j<=s;j++){
                 if(w[i-1]<=j){
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-w[i-1]]+1);
+                    dp[i][j]=(dp[i-1][j] + dp[i][j-w[i-1]]) % MOD;
                 }else{
                     dp[i][j]=dp[i-1][j];
                 }
+                
             }
         }
-        if(dp[n][1000-m]==INT_MIN) cout<<0<<endl;
-        else cout<<dp[n][1000-m]<<endl;
+        if (dp[n][1000-m] == INT_MIN) cout << -1 << endl;
+        else cout << dp[n][1000-m] << endl;
     }
-    
     return 0;
 }
